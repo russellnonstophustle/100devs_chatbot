e7d14dad5c2967f5064ecc8a527c5c1060ce26f3
Chris Russell <crussell8690@gmail.com>
2023-01-09 11:06:59 -0800

    fixed PORT

--- js/index.js
+++ js/index.js
@@ -42,4 +42,5 @@ app.post('/message', (req, res) => {
     });
 });
 
-app.listen(3000, () => console.log('Listening on port 3000'));
+const PORT = process.env.PORT || 3000
+app.listen(PORT, () => console.log(`Listening at http://localhost:${PORT}/`))
